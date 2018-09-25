0e59dcec309a92f5a2c78403cf54fa76f7b6226a
Li Dong <dongli@lasg.iap.ac.cn>
2018-09-25 12:21:23 +0800

    Fix edit_file.py when line matches two patterns

--- utils/edit_file.py
+++ utils/edit_file.py
@@ -5,14 +5,11 @@ def edit_file(filepath, changes):
 	try:
 		with fileinput.FileInput(filepath, inplace=True) as file:
 			for line in file:
-				found = False
+				changed_line = line
 				for change in changes:
-					if re.search(change[0], line, flags=re.IGNORECASE):
-						print(re.sub(change[0], change[1], line, flags=re.IGNORECASE), end='')
-						found = True
-						break
-				if not found:
-					print(line, end='')
+					if re.search(change[0], changed_line, flags=re.IGNORECASE):
+						changed_line = re.sub(change[0], change[1], changed_line, flags=re.IGNORECASE)
+				print(changed_line, end='')
 	except Exception as e:
 		print('[Error]: Failed to edit file {}! {}'.format(filepath, e))
 		exit(1)
