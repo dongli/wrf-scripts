3ef296b03c21f7bdf1f516ff04d7613e06960f00
Li Dong <dongli@lasg.iap.ac.cn>
2018-12-01 16:16:19 +0800

    Refactor codes

--- build_gsi.py
+++ build_gsi.py
@@ -5,179 +5,184 @@ import re
 import os
 import pexpect
 import sys
-sys.path.append('./utils')
+sys.path.append(f'{os.path.dirname(os.path.realpath(__file__))}/utils')
 from utils import edit_file, run, cli, check_files
 
-parser = argparse.ArgumentParser(description="Build WRF model and its friends.\n\nLongrun Weather Inc., NWP operation software.\nCopyright (C) 2018 - All Rights Reserved.", formatter_class=argparse.RawTextHelpFormatter)
-parser.add_argument('-c', '--codes', help='Root directory of all codes (e.g. WRF, GSI)')
-parser.add_argument('-w', '--wrf-root', dest='wrf_root', help='WRF root directory (e.g. WRF)')
-parser.add_argument('-g', '--gsi-root', dest='gsi_root', help='GSI root directory (e.g. GSI)')
-parser.add_argument('-s', '--compiler-suite', dest='compiler_suite', help='Compiler suite', choices=['gnu', 'pgi', 'intel'])
-parser.add_argument('-v', '--verbose', help='Print out build log', action='store_true')
-parser.add_argument('-f', '--force', help='Force to rebuild if already built', action='store_true')
-args = parser.parse_args()
-
-if not args.wrf_root:
-	if os.getenv('WRF_ROOT'):
-		args.wrf_root = os.getenv('WRF_ROOT')
-	elif args.codes:
-		args.wrf_root = args.codes + '/WRF'
-	else:
-		cli.error('Option --wrf-root or environment variable WRF_ROOT need to be set!')
-
-args.wrf_root = os.path.abspath(args.wrf_root)
-
-if not args.gsi_root:
-	if os.getenv('GSI_ROOT'):
-		args.gsi_root = os.getenv('GSI_ROOT')
-	elif args.codes:
-		args.gsi_root = args.codes + '/GSI'
-	else:
-		cli.error('Option --gsi-root or environment variable GSI_ROOT need to be set!')
-
-args.gsi_root = os.path.abspath(args.gsi_root)
-
-if args.compiler_suite == 'gnu':
-	fc = 'gfortran'
-elif args.compiler_suite == 'intel':
-	fc = 'ifort'
-
-# Check environment.
-if not os.getenv('NETCDF'):
-	cli.error('Shell variable NETCDF is not set!')
-if not os.getenv('LAPACK_PATH'):
-	cli.error('Shell variable LAPACK_PATH is not set!')
-
-os.chdir(args.wrf_root)
-expected_exe_files = ('main/wrf.exe', 'main/real.exe', 'main/ndown.exe', 'main/tc.exe')
-if not check_files(expected_exe_files):
-	cli.error('WRF has not been built! Build it first.')
-
-os.chdir(args.gsi_root)
-if args.force: run('rm -rf build')
-if not os.path.isdir('build'): os.mkdir('build')
-os.chdir('build')
-expected_exe_files = (
-	'bin/enkf_gfs.x',
-	'bin/gsi.x',
-	'lib/libbacio_v2.0.1.a',
-	'lib/libbufr_v10.2.5.a',
-	'lib/libcrtm_v2.2.3.a',
-	'lib/libenkfdeplib.a',
-	'lib/libenkflib.a',
-	'lib/libgsilib_shrd.a',
-	'lib/libgsilib_wrf.a',
-	'lib/libnemsio_v2.2.1.a',
-	'lib/libsfcio_v1.1.0.a',
-	'lib/libsigio_v2.0.1.a',
-	'lib/libsp_v2.0.2.a',
-	'lib/libw3emc_v2.2.0.a',
-	'lib/libw3nco_v2.0.6.a'
-)
-if not check_files(expected_exe_files):
-	cli.notice('Fix GSI 3.6!')
-	edit_file('../cmake/Modules/FindCORELIBS.cmake', [
-		['\${CMAKE_SOURCE_DIR}/libsrc', '${CMAKE_SOURCE_DIR}/lib/libsrc']
-	])
-	edit_file('../cmake/Modules/setCompilerFlags.cmake', [
-		['set\(BACIO_Fortran_FLAGS " -O3 -fconvert=big-endian -ffree-form', 'set(BACIO_Fortran_FLAGS " -O3 -fconvert=big-endian']
-	])
-	edit_file('../core-libs/sigio/CMakeLists.txt', [
-		['\*\.f\)', '*.f90)']
-	])
-	edit_file('../src/hybrid_ensemble_isotropic.F90', [
-		['stop\(123\)', 'stop 123']
-	])
-	edit_file('../src/setupoz.f90', [
-		['my_head%ij\(1\),my_head%wij\(1\)\)', 'my_head%ij,my_head%wij)']
-	])
-
-	cli.notice('Configure GSI ...')
+def build_gsi(wrf_root, gsi_root, args):
 	if args.compiler_suite == 'gnu':
-		run(f'CC=gcc CXX=g++ FC=gfortran cmake .. -DBUILD_CORELIBS=ON -DWRFPATH={args.wrf_root} &> cmake.out')
-
-	cli.notice('Compile GSI ...')
-	if args.verbose:
-		run('make')
-	else:
-		run('make &> make.out')
-
-	if check_files(expected_exe_files):
-		cli.notice('Succeeded.')
-	else:
+		fc = 'gfortran'
+	elif args.compiler_suite == 'intel':
+		fc = 'ifort'
+	
+	# Check environment.
+	if not os.getenv('NETCDF'):
+		cli.error('Shell variable NETCDF is not set!')
+	if not os.getenv('LAPACK_PATH'):
+		cli.error('Shell variable LAPACK_PATH is not set!')
+	
+	os.chdir(args.wrf_root)
+	expected_exe_files = ('main/wrf.exe', 'main/real.exe', 'main/ndown.exe', 'main/tc.exe')
+	if not check_files(expected_exe_files):
+		cli.error('WRF has not been built! Build it first.')
+	
+	os.chdir(args.gsi_root)
+	if args.force: run('rm -rf build')
+	if not os.path.isdir('build'): os.mkdir('build')
+	os.chdir('build')
+	expected_exe_files = (
+		'bin/enkf_gfs.x',
+		'bin/gsi.x',
+		'lib/libbacio_v2.0.1.a',
+		'lib/libbufr_v10.2.5.a',
+		'lib/libcrtm_v2.2.3.a',
+		'lib/libenkfdeplib.a',
+		'lib/libenkflib.a',
+		'lib/libgsilib_shrd.a',
+		'lib/libgsilib_wrf.a',
+		'lib/libnemsio_v2.2.1.a',
+		'lib/libsfcio_v1.1.0.a',
+		'lib/libsigio_v2.0.1.a',
+		'lib/libsp_v2.0.2.a',
+		'lib/libw3emc_v2.2.0.a',
+		'lib/libw3nco_v2.0.6.a'
+	)
+	if not check_files(expected_exe_files):
+		cli.notice('Fix GSI 3.6!')
+		edit_file('../cmake/Modules/FindCORELIBS.cmake', [
+			['\${CMAKE_SOURCE_DIR}/libsrc', '${CMAKE_SOURCE_DIR}/lib/libsrc']
+		])
+		edit_file('../cmake/Modules/setCompilerFlags.cmake', [
+			['set\(BACIO_Fortran_FLAGS " -O3 -fconvert=big-endian -ffree-form', 'set(BACIO_Fortran_FLAGS " -O3 -fconvert=big-endian']
+		])
+		edit_file('../core-libs/sigio/CMakeLists.txt', [
+			['\*\.f\)', '*.f90)']
+		])
+		edit_file('../src/hybrid_ensemble_isotropic.F90', [
+			['stop\(123\)', 'stop 123']
+		])
+		edit_file('../src/setupoz.f90', [
+			['my_head%ij\(1\),my_head%wij\(1\)\)', 'my_head%ij,my_head%wij)']
+		])
+	
+		cli.notice('Configure GSI ...')
+		if args.compiler_suite == 'gnu':
+			run(f'CC=gcc CXX=g++ FC=gfortran cmake .. -DBUILD_CORELIBS=ON -DWRFPATH={args.wrf_root} &> cmake.out')
+	
+		cli.notice('Compile GSI ...')
 		if args.verbose:
-			cli.error('Failed')
+			run('make')
 		else:
-			cli.error(f'Failed! Check {args.gsi_root}/build/make.out')
-else:
-	cli.notice('GSI has already been built.')
-
-os.chdir(f'{args.gsi_root}/util/bufr_tools')
-if args.force: run('make clean')
-expected_exe_files = (
-	'bufr_append_sample.exe',
-	'bufr_decode_radiance.exe',
-	'bufr_decode_sample.exe',
-	'bufr_encode_sample.exe',
-	'prepbufr_append_retrieve.exe',
-	'prepbufr_append_surface.exe',
-	'prepbufr_append_upperair.exe',
-	'prepbufr_decode_all.exe',
-	'prepbufr_encode_surface.exe',
-	'prepbufr_encode_upperair.exe',
-	'prepbufr_inventory.exe'
-)
-if not check_files(expected_exe_files):
-	edit_file('makefile', [
-		['^\s*FC\s*=.*$', f'FC = {fc}'],
-		['-I\.\./\.\./dtc', '-I../../build'],
-		['-L\.\./\.\./dtc', '-L../../build'],
-		['-lbufr_i4r8', '-lbufr_v10.2.5']
-	])
-
-	cli.notice('Compile bufr_tools ...')
-	if args.verbose:
-		run('make')
+			run('make &> make.out')
+	
+		if check_files(expected_exe_files):
+			cli.notice('Succeeded.')
+		else:
+			if args.verbose:
+				cli.error('Failed')
+			else:
+				cli.error(f'Failed! Check {args.gsi_root}/build/make.out')
 	else:
-		run('make &> make.out')
-
-	if check_files(expected_exe_files):
-		cli.notice('Succeeded.')
-	else:
+		cli.notice('GSI has already been built.')
+	
+	os.chdir(f'{args.gsi_root}/util/bufr_tools')
+	if args.force: run('make clean')
+	expected_exe_files = (
+		'bufr_append_sample.exe',
+		'bufr_decode_radiance.exe',
+		'bufr_decode_sample.exe',
+		'bufr_encode_sample.exe',
+		'prepbufr_append_retrieve.exe',
+		'prepbufr_append_surface.exe',
+		'prepbufr_append_upperair.exe',
+		'prepbufr_decode_all.exe',
+		'prepbufr_encode_surface.exe',
+		'prepbufr_encode_upperair.exe',
+		'prepbufr_inventory.exe'
+	)
+	if not check_files(expected_exe_files):
+		edit_file('makefile', [
+			['^\s*FC\s*=.*$', f'FC = {fc}'],
+			['-I\.\./\.\./dtc', '-I../../build'],
+			['-L\.\./\.\./dtc', '-L../../build'],
+			['-lbufr_i4r8', '-lbufr_v10.2.5']
+		])
+	
+		cli.notice('Compile bufr_tools ...')
 		if args.verbose:
-			cli.error('Failed!')
+			run('make')
 		else:
-			cli.error(f'Failed! Check {args.gsi_root}/util/bufr_tools/make.out')
-else:
-	cli.notice('GSI bufr_tools has been built.')
-
-os.chdir(f'{args.gsi_root}/util/Analysis_Utilities/read_diag/')
-expected_exe_files = (
-	'read_diag_conv.exe',
-	'read_diag_conv_ens.exe',
-	'read_diag_rad.exe'
-)
-if not check_files(expected_exe_files):
-	edit_file('makefile', [
-		['include \.\./\.\./\.\./dtc/configure.gsi', ''],
-		['\$\(SFC\)', fc],
-		['-I\.\./\.\./\.\./dtc', '-I../../../build'],
-		['-L\.\./\.\./\.\./src -lgsi', '-L../../../build/lib -lgsilib_shrd'],
-		['FLAGS= \$\(FFLAGS_DEFAULT\)', 'FLAGS = -fconvert=big-endian']
-	])
-
-	cli.notice('Compile read_diag ...')
-	if args.verbose:
-		run('make')
+			run('make &> make.out')
+	
+		if check_files(expected_exe_files):
+			cli.notice('Succeeded.')
+		else:
+			if args.verbose:
+				cli.error('Failed!')
+			else:
+				cli.error(f'Failed! Check {args.gsi_root}/util/bufr_tools/make.out')
 	else:
-		run('make &> make.out')
-
-	if check_files(expected_exe_files):
-		cli.notice('Succeeded.')
-	else:
+		cli.notice('GSI bufr_tools has been built.')
+	
+	os.chdir(f'{args.gsi_root}/util/Analysis_Utilities/read_diag/')
+	expected_exe_files = (
+		'read_diag_conv.exe',
+		'read_diag_conv_ens.exe',
+		'read_diag_rad.exe'
+	)
+	if not check_files(expected_exe_files):
+		edit_file('makefile', [
+			['include \.\./\.\./\.\./dtc/configure.gsi', ''],
+			['\$\(SFC\)', fc],
+			['-I\.\./\.\./\.\./dtc', '-I../../../build'],
+			['-L\.\./\.\./\.\./src -lgsi', '-L../../../build/lib -lgsilib_shrd'],
+			['FLAGS= \$\(FFLAGS_DEFAULT\)', 'FLAGS = -fconvert=big-endian']
+		])
+	
+		cli.notice('Compile read_diag ...')
 		if args.verbose:
-			cli.error('Failed')
+			run('make')
 		else:
-			cli.error(f'Failed! Check {args.gsi_root}/util/Analysis_Utilities/read_diag/make.out')
-else:
-	cli.notice('GSI read_diag has been built.')
+			run('make &> make.out')
+	
+		if check_files(expected_exe_files):
+			cli.notice('Succeeded.')
+		else:
+			if args.verbose:
+				cli.error('Failed')
+			else:
+				cli.error(f'Failed! Check {args.gsi_root}/util/Analysis_Utilities/read_diag/make.out')
+	else:
+		cli.notice('GSI read_diag has been built.')
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description="Build WRF model and its friends.\n\nLongrun Weather Inc., NWP operation software.\nCopyright (C) 2018 - All Rights Reserved.", formatter_class=argparse.RawTextHelpFormatter)
+	parser.add_argument('-c', '--codes', help='Root directory of all codes (e.g. WRF, GSI)')
+	parser.add_argument('-w', '--wrf-root', dest='wrf_root', help='WRF root directory (e.g. WRF)')
+	parser.add_argument('-g', '--gsi-root', dest='gsi_root', help='GSI root directory (e.g. GSI)')
+	parser.add_argument('-s', '--compiler-suite', dest='compiler_suite', help='Compiler suite', choices=['gnu', 'pgi', 'intel'])
+	parser.add_argument('-v', '--verbose', help='Print out build log', action='store_true')
+	parser.add_argument('-f', '--force', help='Force to rebuild if already built', action='store_true')
+	args = parser.parse_args()
+	
+	if not args.wrf_root:
+		if os.getenv('WRF_ROOT'):
+			args.wrf_root = os.getenv('WRF_ROOT')
+		elif args.codes:
+			args.wrf_root = args.codes + '/WRF'
+		else:
+			cli.error('Option --wrf-root or environment variable WRF_ROOT need to be set!')
+	
+	args.wrf_root = os.path.abspath(args.wrf_root)
+	
+	if not args.gsi_root:
+		if os.getenv('GSI_ROOT'):
+			args.gsi_root = os.getenv('GSI_ROOT')
+		elif args.codes:
+			args.gsi_root = args.codes + '/GSI'
+		else:
+			cli.error('Option --gsi-root or environment variable GSI_ROOT need to be set!')
+	
+	args.gsi_root = os.path.abspath(args.gsi_root)
+
+	build_gsi(args.wrf_root, args.gsi_root, args)
+
--- build_wrf.py
+++ build_wrf.py
@@ -4,186 +4,196 @@ import argparse
 import re
 import os
 import pexpect
+import platform
 import sys
-sys.path.append('./utils')
+sys.path.append(f'{os.path.dirname(os.path.realpath(__file__))}/utils')
 from utils import edit_file, run, cli, check_files
 
-parser = argparse.ArgumentParser(description="Build WRF model and its friends.\n\nLongrun Weather Inc., NWP operation software.\nCopyright (C) 2018 - All Rights Reserved.", formatter_class=argparse.RawTextHelpFormatter)
-parser.add_argument('-c', '--codes', help='Root directory of all codes (e.g. WRFV3, WPS)')
-parser.add_argument('-w', '--wrf-root', dest='wrf_root', help='WRF root directory (e.g. WRFV3)')
-parser.add_argument('-p', '--wps-root', dest='wps_root', help='WPS root directory (e.g. WPS)')
-parser.add_argument('-d', '--wrfda-root', dest='wrfda_root', help='WRFDA root directory (e.g. WRF for V4)')
-parser.add_argument('-b', '--use-hyb', dest='use_hyb', help='Use hybrid vertical coordinate', action='store_true')
-parser.add_argument('-g', '--use-grib', dest='use_grib', help='Use GRIB IO capability of WRF', action='store_true')
-parser.add_argument('-s', '--compiler-suite', dest='compiler_suite', help='Compiler suite', choices=['gnu', 'pgi', 'intel'])
-parser.add_argument('-f', '--force', help='Force to rebuild if already built', action='store_true')
-args = parser.parse_args()
-
-if not args.wrf_root:
-	if os.getenv('WRF_ROOT'):
-		args.wrf_root = os.getenv('WRF_ROOT')
-	elif args.codes:
-		args.wrf_root = args.codes + '/WRF'
-	else:
-		cli.error('Option --wrf-root or environment variable WRF_ROOT need to be set!')
-
-if not args.wps_root:
-	if os.getenv('WPS_ROOT'):
-		args.wps_root = os.getenv('WPS_ROOT')
-	elif args.codes:
-		args.wps_root = args.codes + '/WPS'
-	else:
-		cli.error('Option --wps-root or environment variable WPS_ROOT need to be set!')
-
-if not args.wrfda_root:
-	if os.getenv('WRFDA_ROOT'):
-		args.wrfda_root = os.getenv('WRFDA_ROOT')
-	elif args.codes:
-		args.wrfda_root = args.codes + '/WRFDA'
-	else:
-		cli.error('Option --wrfda-root or environment variable WRFDA_ROOT need to be set!')
-
-os.chdir(args.wrf_root)
-if args.force: run('./clean -a &> /dev/null')
-expected_exe_files = ('main/wrf.exe', 'main/real.exe', 'main/ndown.exe', 'main/tc.exe')
-if not check_files(expected_exe_files):
-	cli.notice('Configure WRF ...')
-	if args.use_grib:
-		cli.notice('Set GRIB2 flag.')
-		edit_file('./arch/Config.pl', [
-			['\$I_really_want_to_output_grib2_from_WRF = "FALSE"', '$I_really_want_to_output_grib2_from_WRF = "TRUE"']
-		])
-	if args.use_hyb:
-		child = pexpect.spawn('./configure -hyb')
-	else:
-		child = pexpect.spawn('./configure')
-	child.expect('Enter selection.*')
-	if args.compiler_suite == 'intel':
-		child.sendline('15')
-	elif args.compiler_suite == 'gnu':
-		child.sendline('34')
-	elif args.compiler_suite == 'pgi':
-		child.sendline('54')
-	child.expect('Compile for nesting.*')
-	child.sendline('1')
-	child.wait()
-
-	if args.compiler_suite == 'pgi':
-		edit_file('./configure.wrf', [
-			['pgf90', 'pgfortran'],
-			['mpif90', 'mpifort']
-		])
-
-	cli.notice('Compile WRF ...')
-	run('./compile em_real &> compile.out')
+def build_wrf(wrf_root, wps_root, wrfda_root, args):
+	os.chdir(wrf_root)
+	if args.force: run('./clean -a &> /dev/null')
+	expected_exe_files = ('main/wrf.exe', 'main/real.exe', 'main/ndown.exe', 'main/tc.exe')
+	if not check_files(expected_exe_files):
+		cli.notice('Configure WRF ...')
+		if args.use_grib:
+			cli.notice('Set GRIB2 flag.')
+			edit_file('./arch/Config.pl', [
+				['\$I_really_want_to_output_grib2_from_WRF = "FALSE"', '$I_really_want_to_output_grib2_from_WRF = "TRUE"']
+			])
+		if args.use_hyb:
+			child = pexpect.spawn('./configure -hyb', encoding='utf-8')
+		else:
+			child = pexpect.spawn('./configure', encoding='utf-8')
+		child.logfile = sys.stdout
+		child.expect('Enter selection.*')
+		if platform.system() == 'Darwin':
+			if args.compiler_suite == 'gnu':
+				child.sendline('15')
+		else:
+			if args.compiler_suite == 'intel':
+				child.sendline('15')
+			elif args.compiler_suite == 'gnu':
+				child.sendline('34')
+			elif args.compiler_suite == 'pgi':
+				child.sendline('54')
+		child.expect('Compile for nesting.*:')
+		child.sendline('1')
+		if platform.system() == 'Darwin': child.expect('This build of WRF will use NETCDF4 with HDF5 compression')
+		child.wait()
 	
-	if check_files(expected_exe_files):
-		cli.notice('Succeeded.')
+		if args.compiler_suite == 'pgi':
+			edit_file('./configure.wrf', [
+				['pgf90', 'pgfortran'],
+				['mpif90', 'mpifort']
+			])
+	
+		cli.notice('Compile WRF ...')
+		run('./compile em_real &> compile.out')
+		
+		if check_files(expected_exe_files):
+			cli.notice('Succeeded.')
+		else:
+			cli.error(f'Failed! Check {wrf_root}/compile.out')
 	else:
-		cli.error(f'Failed! Check {args.wrf_root}/compile.out')
-else:
-	cli.notice('WRF is already built.')
-
-os.chdir(args.wps_root)
-if args.force: run('./clean -a &> /dev/null')
-expected_exe_files = ('geogrid/src/geogrid.exe', 'metgrid/src/metgrid.exe', 'ungrib/src/ungrib.exe')
-if not check_files(expected_exe_files):
-	cli.notice('Configure WPS ...')
-	child = pexpect.spawn('./configure')
-	child.expect('Enter selection.*')
-	if args.compiler_suite == 'intel':
-		child.sendline('19')
-	elif args.compiler_suite == 'gnu':
-		child.sendline('3')
-	elif args.compiler_suite == 'pgi':
-		child.sendline('7')
-	child.wait()
-
-	if args.compiler_suite == 'pgi':
-		edit_file('./configure.wrf', [
-			['pgf90', 'pgfortran'],
-			['mpif90', 'mpifort']
-		])
-
-	run('sed -i "s/mpicc -cc=.*/mpicc/" configure.wps')
-	run('sed -i "s/mpif90 -f90=.*/mpif90/" configure.wps')
-
-	cli.notice('Compile WPS ...')
-	run('./compile &> compile.out')
-
-	if check_files(expected_exe_files):
-		cli.notice('Succeeded.')
+		cli.notice('WRF is already built.')
+	
+	os.chdir(wps_root)
+	if args.force: run('./clean -a &> /dev/null')
+	expected_exe_files = ('geogrid/src/geogrid.exe', 'metgrid/src/metgrid.exe', 'ungrib/src/ungrib.exe')
+	if not check_files(expected_exe_files):
+		cli.notice('Configure WPS ...')
+		child = pexpect.spawn('./configure')
+		child.expect('Enter selection.*')
+		if args.compiler_suite == 'intel':
+			child.sendline('19')
+		elif args.compiler_suite == 'gnu':
+			child.sendline('3')
+		elif args.compiler_suite == 'pgi':
+			child.sendline('7')
+		child.wait()
+	
+		if args.compiler_suite == 'pgi':
+			edit_file('./configure.wrf', [
+				['pgf90', 'pgfortran'],
+				['mpif90', 'mpifort']
+			])
+	
+		run('sed -i "s/mpicc -cc=.*/mpicc/" configure.wps')
+		run('sed -i "s/mpif90 -f90=.*/mpif90/" configure.wps')
+	
+		cli.notice('Compile WPS ...')
+		run('./compile &> compile.out')
+	
+		if check_files(expected_exe_files):
+			cli.notice('Succeeded.')
+		else:
+			cli.error(f'Failed! Check {wps_root}/compile.out')
 	else:
-		cli.error(f'Failed! Check {args.wps_root}/compile.out')
-else:
-	cli.notice('WPS is already built.')
-
-os.chdir(args.wrfda_root)
-if args.force: run('./clean -a &> /dev/null')
-expected_exe_files = (
-	'var/build/da_advance_time.exe',
-	'var/build/da_bias_airmass.exe',
-	'var/build/da_bias_scan.exe',
-	'var/build/da_bias_sele.exe',
-	'var/build/da_bias_verif.exe',
-	'var/build/da_rad_diags.exe',
-	'var/build/da_tune_obs_desroziers.exe',
-	'var/build/da_tune_obs_hollingsworth1.exe',
-	'var/build/da_tune_obs_hollingsworth2.exe',
-	'var/build/da_update_bc_ad.exe',
-	'var/build/da_update_bc.exe',
-	'var/build/da_verif_grid.exe',
-	'var/build/da_verif_obs.exe',
-	'var/build/da_wrfvar.exe',
-	'var/build/gen_be_addmean.exe',
-	'var/build/gen_be_cov2d3d_contrib.exe',
-	'var/build/gen_be_cov2d.exe',
-	'var/build/gen_be_cov3d2d_contrib.exe',
-	'var/build/gen_be_cov3d3d_bin3d_contrib.exe',
-	'var/build/gen_be_cov3d3d_contrib.exe',
-	'var/build/gen_be_cov3d.exe',
-	'var/build/gen_be_diags.exe',
-	'var/build/gen_be_diags_read.exe',
-	'var/build/gen_be_ensmean.exe',
-	'var/build/gen_be_ensrf.exe',
-	'var/build/gen_be_ep1.exe',
-	'var/build/gen_be_ep2.exe',
-	'var/build/gen_be_etkf.exe',
-	'var/build/gen_be_hist.exe',
-	'var/build/gen_be_stage0_gsi.exe',
-	'var/build/gen_be_stage0_wrf.exe',
-	'var/build/gen_be_stage1_1dvar.exe',
-	'var/build/gen_be_stage1.exe',
-	'var/build/gen_be_stage1_gsi.exe',
-	'var/build/gen_be_stage2_1dvar.exe',
-	'var/build/gen_be_stage2a.exe',
-	'var/build/gen_be_stage2.exe',
-	'var/build/gen_be_stage2_gsi.exe',
-	'var/build/gen_be_stage3.exe',
-	'var/build/gen_be_stage4_global.exe',
-	'var/build/gen_be_stage4_regional.exe',
-	'var/build/gen_be_vertloc.exe',
-	'var/build/gen_mbe_stage2.exe',
-	'var/obsproc/src/obsproc.exe')
-if not check_files(expected_exe_files):
-	cli.notice('Configure WRFDA ...')
-	child = pexpect.spawn('./configure wrfda')
-	child.expect('Enter selection.*')
-	if args.compiler_suite == 'intel':
-		child.sendline('15')
-	elif args.compiler_suite == 'gnu':
-		child.sendline('34')
-	elif args.compiler_suite == 'pgi':
-		child.sendline('54')
-	child.wait()
-
-	cli.notice('Compile WRFDA ...')
-	run('./compile all_wrfvar &> compile.wrfvar.out')
-
-	if check_files(expected_exe_files, fatal=True):
-		cli.notice('Succeeded.')
+		cli.notice('WPS is already built.')
+	
+	os.chdir(wrfda_root)
+	if args.force: run('./clean -a &> /dev/null')
+	expected_exe_files = (
+		'var/build/da_advance_time.exe',
+		'var/build/da_bias_airmass.exe',
+		'var/build/da_bias_scan.exe',
+		'var/build/da_bias_sele.exe',
+		'var/build/da_bias_verif.exe',
+		'var/build/da_rad_diags.exe',
+		'var/build/da_tune_obs_desroziers.exe',
+		'var/build/da_tune_obs_hollingsworth1.exe',
+		'var/build/da_tune_obs_hollingsworth2.exe',
+		'var/build/da_update_bc_ad.exe',
+		'var/build/da_update_bc.exe',
+		'var/build/da_verif_grid.exe',
+		'var/build/da_verif_obs.exe',
+		'var/build/da_wrfvar.exe',
+		'var/build/gen_be_addmean.exe',
+		'var/build/gen_be_cov2d3d_contrib.exe',
+		'var/build/gen_be_cov2d.exe',
+		'var/build/gen_be_cov3d2d_contrib.exe',
+		'var/build/gen_be_cov3d3d_bin3d_contrib.exe',
+		'var/build/gen_be_cov3d3d_contrib.exe',
+		'var/build/gen_be_cov3d.exe',
+		'var/build/gen_be_diags.exe',
+		'var/build/gen_be_diags_read.exe',
+		'var/build/gen_be_ensmean.exe',
+		'var/build/gen_be_ensrf.exe',
+		'var/build/gen_be_ep1.exe',
+		'var/build/gen_be_ep2.exe',
+		'var/build/gen_be_etkf.exe',
+		'var/build/gen_be_hist.exe',
+		'var/build/gen_be_stage0_gsi.exe',
+		'var/build/gen_be_stage0_wrf.exe',
+		'var/build/gen_be_stage1_1dvar.exe',
+		'var/build/gen_be_stage1.exe',
+		'var/build/gen_be_stage1_gsi.exe',
+		'var/build/gen_be_stage2_1dvar.exe',
+		'var/build/gen_be_stage2a.exe',
+		'var/build/gen_be_stage2.exe',
+		'var/build/gen_be_stage2_gsi.exe',
+		'var/build/gen_be_stage3.exe',
+		'var/build/gen_be_stage4_global.exe',
+		'var/build/gen_be_stage4_regional.exe',
+		'var/build/gen_be_vertloc.exe',
+		'var/build/gen_mbe_stage2.exe',
+		'var/obsproc/src/obsproc.exe')
+	if not check_files(expected_exe_files):
+		cli.notice('Configure WRFDA ...')
+		child = pexpect.spawn('./configure wrfda')
+		child.expect('Enter selection.*')
+		if args.compiler_suite == 'intel':
+			child.sendline('15')
+		elif args.compiler_suite == 'gnu':
+			child.sendline('34')
+		elif args.compiler_suite == 'pgi':
+			child.sendline('54')
+		child.wait()
+	
+		cli.notice('Compile WRFDA ...')
+		run('./compile all_wrfvar &> compile.wrfvar.out')
+	
+		if check_files(expected_exe_files, fatal=True):
+			cli.notice('Succeeded.')
+		else:
+			cli.error(f'Failed! Check {wrfda_root}/compile.out')
 	else:
-		cli.error(f'Failed! Check {args.wrfda_root}/compile.out')
-else:
-	cli.notice('WRFDA is already built.')
+		cli.notice('WRFDA is already built.')
 
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description="Build WRF model and its friends.\n\nLongrun Weather Inc., NWP operation software.\nCopyright (C) 2018 - All Rights Reserved.", formatter_class=argparse.RawTextHelpFormatter)
+	parser.add_argument('-c', '--codes', help='Root directory of all codes (e.g. WRFV3, WPS)')
+	parser.add_argument('-w', '--wrf-root', dest='wrf_root', help='WRF root directory (e.g. WRFV3)')
+	parser.add_argument('-p', '--wps-root', dest='wps_root', help='WPS root directory (e.g. WPS)')
+	parser.add_argument('-d', '--wrfda-root', dest='wrfda_root', help='WRFDA root directory (e.g. WRF for V4)')
+	parser.add_argument('-b', '--use-hyb', dest='use_hyb', help='Use hybrid vertical coordinate', action='store_true')
+	parser.add_argument('-g', '--use-grib', dest='use_grib', help='Use GRIB IO capability of WRF', action='store_true')
+	parser.add_argument('-s', '--compiler-suite', dest='compiler_suite', help='Compiler suite', choices=['gnu', 'pgi', 'intel'])
+	parser.add_argument('-f', '--force', help='Force to rebuild if already built', action='store_true')
+	args = parser.parse_args()
+	
+	if not args.wrf_root:
+		if os.getenv('WRF_ROOT'):
+			args.wrf_root = os.getenv('WRF_ROOT')
+		elif args.codes:
+			args.wrf_root = args.codes + '/WRF'
+		else:
+			cli.error('Option --wrf-root or environment variable WRF_ROOT need to be set!')
+	
+	if not args.wps_root:
+		if os.getenv('WPS_ROOT'):
+			args.wps_root = os.getenv('WPS_ROOT')
+		elif args.codes:
+			args.wps_root = args.codes + '/WPS'
+		else:
+			cli.error('Option --wps-root or environment variable WPS_ROOT need to be set!')
+	
+	if not args.wrfda_root:
+		if os.getenv('WRFDA_ROOT'):
+			args.wrfda_root = os.getenv('WRFDA_ROOT')
+		elif args.codes:
+			args.wrfda_root = args.codes + '/WRFDA'
+		else:
+			cli.error('Option --wrfda-root or environment variable WRFDA_ROOT need to be set!')
+
+	build_wrf(args.wrf_root, args.wps_root, args.wrfda_root, args)
