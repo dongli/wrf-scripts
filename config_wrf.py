49ad226e8dd93afaefe2cf5e7fc0b0f2d256aa4d
Li Dong <dongli@lasg.iap.ac.cn>
2018-12-18 02:29:56 +0000

    Enhance run_wps.py to handle ECMWF, but not success yet

--- config_wrf.py
+++ config_wrf.py
@@ -7,13 +7,13 @@ import pendulum
 import f90nml
 import re
 from math import radians, cos, sin, asin, sqrt
-from shutil import copyfile
+from shutil import copy
 from pprint import pprint
 import sys
 sys.path.append(f'{os.path.dirname(os.path.realpath(__file__))}/utils')
 from utils import cli, parse_config
 
-def config_wrf(wrf_root, wps_root, geog_root, config):
+def config_wrf(work_root, wrf_root, wps_root, geog_root, config):
 	pprint(config)
 	common_config = config['common']
 
@@ -25,33 +25,40 @@ def config_wrf(wrf_root, wps_root, geog_root, config):
 	start_time_str = start_time.format('YYYY-MM-DD_HH:mm:ss')
 	end_time_str = end_time.format('YYYY-MM-DD_HH:mm:ss')
 
-	os.chdir(wps_root)
+	wps_work_dir = work_root + '/WPS'
+	if not os.path.isdir(wps_work_dir): os.mkdir(wps_work_dir)
+	os.chdir(wps_work_dir)
 
 	cli.notice('Edit namelist.wps for WPS.')
-	namelist_wps = f90nml.read('./namelist.wps')
-	namelist_wps['share']  ['max_dom']           = max_dom
-	namelist_wps['share']  ['start_date']        = [start_time_str for i in range(max_dom)]
-	namelist_wps['share']  ['end_date']          = [end_time_str if i == 0 else start_time_str for i in range(max_dom)]
-	namelist_wps['geogrid']['parent_id']         = common_config['parent_id']
-	namelist_wps['geogrid']['parent_grid_ratio'] = common_config['parent_grid_ratio']
-	namelist_wps['geogrid']['i_parent_start']    = common_config['i_parent_start']
-	namelist_wps['geogrid']['j_parent_start']    = common_config['j_parent_start']
-	namelist_wps['geogrid']['e_we']              = common_config['e_we']
-	namelist_wps['geogrid']['e_sn']              = common_config['e_sn']
-	namelist_wps['geogrid']['dx']                = common_config['resolution']
-	namelist_wps['geogrid']['dy']                = common_config['resolution']
-	namelist_wps['geogrid']['ref_lat']           = common_config['ref_lat']
-	namelist_wps['geogrid']['ref_lon']           = common_config['ref_lon']
-	namelist_wps['geogrid']['truelat1']          = common_config['truelat1']
-	namelist_wps['geogrid']['truelat2']          = common_config['truelat2']
-	namelist_wps['geogrid']['stand_lon']         = common_config['stand_lon']
-	namelist_wps['geogrid']['geog_data_path']    = geog_root
+	copy(f'{wps_root}/namelist.wps', 'namelist.wps')
+	namelist_wps = f90nml.read('namelist.wps')
+	namelist_wps['share']  ['max_dom']              = max_dom
+	namelist_wps['share']  ['start_date']           = [start_time_str for i in range(max_dom)]
+	namelist_wps['share']  ['end_date']             = [end_time_str if i == 0 else start_time_str for i in range(max_dom)]
+	namelist_wps['geogrid']['parent_id']            = common_config['parent_id']
+	namelist_wps['geogrid']['parent_grid_ratio']    = common_config['parent_grid_ratio']
+	namelist_wps['geogrid']['i_parent_start']       = common_config['i_parent_start']
+	namelist_wps['geogrid']['j_parent_start']       = common_config['j_parent_start']
+	namelist_wps['geogrid']['e_we']                 = common_config['e_we']
+	namelist_wps['geogrid']['e_sn']                 = common_config['e_sn']
+	namelist_wps['geogrid']['dx']                   = common_config['resolution']
+	namelist_wps['geogrid']['dy']                   = common_config['resolution']
+	namelist_wps['geogrid']['ref_lat']              = common_config['ref_lat']
+	namelist_wps['geogrid']['ref_lon']              = common_config['ref_lon']
+	namelist_wps['geogrid']['truelat1']             = common_config['truelat1']
+	namelist_wps['geogrid']['truelat2']             = common_config['truelat2']
+	namelist_wps['geogrid']['stand_lon']            = common_config['stand_lon']
+	namelist_wps['geogrid']['geog_data_path']       = geog_root
+	namelist_wps['geogrid']['opt_geogrid_tbl_path'] = wps_work_dir
 	namelist_wps.write('./namelist.wps', force=True)
-	
-	os.chdir(wrf_root + '/run')
-	
+
+	wrf_work_dir = work_root + '/WRF'
+	if not os.path.isdir(wrf_work_dir): os.mkdir(wrf_work_dir)
+	os.chdir(wrf_work_dir)
+
 	cli.notice('Edit namelist.input for WRF.')
-	namelist_input = f90nml.read('./namelist.input')
+	copy(f'{wrf_root}/run/namelist.input', 'namelist.input')
+	namelist_input = f90nml.read('namelist.input')
 	namelist_input['time_control']['run_hours']              = common_config['forecast_hours']
 	namelist_input['time_control']['start_year']             = [int(start_time.format("Y")) for i in range(max_dom)]
 	namelist_input['time_control']['start_month']            = [int(start_time.format("M")) for i in range(max_dom)]
@@ -80,14 +87,24 @@ def config_wrf(wrf_root, wps_root, geog_root, config):
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description="Configure WRF model.\n\nLongrun Weather Inc., NWP operation software.\nCopyright (C) 2018 - All Rights Reserved.", formatter_class=argparse.RawTextHelpFormatter)
 	parser.add_argument('-c', '--codes', help='Root directory of all codes (e.g. WRF, WPS)')
-	parser.add_argument('-w', '--wrf-root', dest='wrf_root', help='WRF root directory (e.g. WRF)')
+	parser.add_argument('-w', '--work-root',  dest='work_root', help='Work root directory')
+	parser.add_argument('-m', '--wrf-root', dest='wrf_root', help='WRF root directory (e.g. WRF)')
 	parser.add_argument('-p', '--wps-root', dest='wps_root', help='WPS root directory (e.g. WPS)')
 	parser.add_argument('-g', '--geog-root', dest='geog_root', help='GEOG data root directory (e.g. WPS_GEOG)')
 	parser.add_argument('-d', '--wrfda-root', dest='wrfda_root', help='WRFDA root directory (e.g. WRFDA)')
 	parser.add_argument('-j', '--config-json', dest='config_json', help='Configuration JSON file.')
 	parser.add_argument('-f', '--force', help='Force to run', action='store_true')
 	args = parser.parse_args()
-	
+
+	if not args.work_root:
+		if os.getenv('WORK_ROOT'):
+			args.work_root = os.getenv('WORK_ROOT')
+		else:
+			cli.error('Option --work-root or environment variable WORK_ROOT need to be set!')
+	args.work_root = os.path.abspath(args.work_root)
+	if not os.path.isdir(args.work_root):
+		cli.error(f'Directory {args.work_root} does not exist!')
+
 	if not args.wrf_root:
 		if os.getenv('WRF_ROOT'):
 			args.wrf_root = os.getenv('WRF_ROOT')
@@ -120,4 +137,4 @@ if __name__ == '__main__':
 	
 	config = parse_config(args.config_json)
 
-	config_wrf(args.wrf_root, args.wps_root, args.geog_root, config)
+	config_wrf(args.work_root, args.wrf_root, args.wps_root, args.geog_root, config)
--- run_wps.py
+++ run_wps.py
@@ -4,66 +4,102 @@ import argparse
 from glob import glob
 import os
 import pendulum
+from jinja2 import Template
 import re
-from shutil import copyfile
+from shutil import copy
 import sys
 sys.path.append(f'{os.path.dirname(os.path.realpath(__file__))}/utils')
 from utils import cli, check_files, run, parse_config
 
-def run_wps(wps_root, gfs_root, config, args):
+def run_wps(work_root, wps_root, bkg_root, config, args):
 	common_config = config['common']
-	
-	os.chdir(args.wps_root)
-	
+
+	if 'type' in common_config['background']:
+		bkg_type = common_config['background']['type']
+	else:
+		bkg_type = 'gfs'
+
+	wps_work_dir = os.path.abspath(args.work_root) + '/WPS'
+	if not os.path.isdir(wps_work_dir): os.mkdir(wps_work_dir)
+	os.chdir(wps_work_dir)
+
+	# ------------------------------------------------------------------------------------------------
+	#                                          GEOGRID
 	cli.notice('Run geogrid.exe ...')
+	copy(f'{wps_root}/geogrid/GEOGRID.TBL.ARW', 'GEOGRID.TBL')
 	expected_files = ['geo_em.d{:02d}.nc'.format(i + 1) for i in range(common_config['max_dom'])]
 	if not check_files(expected_files) or args.force:
 		run('rm -f geo_em.d*.nc')
 		if args.verbose:
-			run('./geogrid.exe')
+			run(f'{wps_root}/geogrid/src/geogrid.exe')
 		else:
-			run('./geogrid.exe &> geogrid.out')
+			run(f'{wps_root}/geogrid/src/geogrid.exe &> geogrid.out')
 		if not check_files(expected_files):
 			if args.verbose:
 				cli.error(f'Failed!')
 			else:
-				cli.error(f'Failed! Check output {os.path.abspath(wps_root)}/geogrid.out.')
+				cli.error(f'Failed! Check output {os.path.abspath(wps_work_dir)}/geogrid.out.')
 		cli.notice('Succeeded.')
 	else:
 		cli.notice('File geo_em.*.nc already exist.')
-	run(f'ls -l {wps_root}/geo_em.*.nc')
-	
+	run(f'ls -l {wps_work_dir}/geo_em.*.nc')
+
+	# ------------------------------------------------------------------------------------------------
+	#                                          UNGRIB
 	cli.notice('Run ungrib.exe ...')
-	# Find out suitable GFS data that cover forecast time period.
-	def is_gfs_exist(date, hour):
-		dir_name = f'{gfs_root}/gfs.{date.format("YYYYMMDDHH")}'
-		file_name = 'gfs.t{:02d}z.pgrb2.0p25.f{:03d}'.format(date.hour, hour)
-		return os.path.isfile(f'{dir_name}/{file_name}')
-	
+	if 'vtable' in common_config['background']:
+		run(f'ln -sf {common_config["background"]["vtable"]} {wps_work_dir}/Vtable')
+	else:
+		run(f'ln -sf {wps_root}/ungrib/Variable_Tables/Vtable.{bkg_type.upper()} {wps_work_dir}/Vtable')
+
+	def eval_bkg_dir(bkg_start_time):
+		return bkg_root + '/' + Template(common_config['background']['dir_pattern']).render(bkg_start_time=bkg_start_time)
+
+	# Find out suitable background data that cover forecast time period.
+	def is_bkg_exist(bkg_start_time):
+		bkg_dir = eval_bkg_dir(bkg_start_time)
+		file_name = Template(common_config['background']['file_pattern']).render(bkg_start_time=bkg_start_time)
+		return len(glob(f'{bkg_dir}/{file_name}')) != 0
+
 	found = False
 	for date in (common_config['start_time'], common_config['start_time'].subtract(days=1)):
 		if found: break
 		for hour in (18, 12, 6, 0):
-			if ((common_config['start_time'] - date).days == 1 or common_config['start_time'].hour >= hour) and is_gfs_exist(date, hour):
-				gfs_start_date = pendulum.datetime(date.year, date.month, date.day, hour)
+			bkg_start_time = pendulum.datetime(date.year, date.month, date.day, hour)
+			if ((common_config['start_time'] - date).days == 1 or common_config['start_time'].hour >= hour) and is_bkg_exist(bkg_start_time):
 				found = True
 				break
-	if not found:
-		cli.error('GFS data is not available!')
-	
+	if not found: cli.error('Background data is not available!')
+	cli.notice(f'Use background starting from {bkg_start_time}.')
+
+	# Generate the background times.
+	bkg_dir = eval_bkg_dir(bkg_start_time)
 	interval_seconds = int(re.search('interval_seconds\s*=\s*(\d+)', open('./namelist.wps').read())[1])
-	run('ln -sf ungrib/Variable_Tables/Vtable.GFS Vtable')
-	gfs_dates = [gfs_start_date]
-	while gfs_dates[len(gfs_dates) - 1] < common_config['end_time']:
-		gfs_dates.append(gfs_dates[len(gfs_dates) - 1].add(seconds=interval_seconds))
-	expected_files = [f'FILE:{date.format("YYYY-MM-DD_HH")}' for date in gfs_dates]
+	bkg_times = [bkg_start_time]
+	while bkg_times[len(bkg_times) - 1] < common_config['end_time']:
+		bkg_times.append(bkg_times[len(bkg_times) - 1].add(seconds=interval_seconds))
+
+	expected_files = [f'FILE:{time.format("YYYY-MM-DD_HH")}' for time in bkg_times]
 	if not check_files(expected_files) or args.force:
-		run('rm -f FILE:*')
-		run(f'./link_grib.csh {gfs_root}/gfs.{gfs_start_date.format("YYYYMMDDHH")}/*')
-		if args.verbose:
-			run('./ungrib.exe')
+		run('rm -f GRIBFILE.* FILE:*')
+		if 'file_processes' in common_config['background']:
+			if not os.path.isdir(f'{wps_work_dir}/background'): os.mkdir(f'{wps_work_dir}/background')
+			os.chdir(f'{wps_work_dir}/background')
+			for bkg_time in bkg_times:
+				try:
+					bkg_file = glob(bkg_dir + '/' + Template(common_config['background']['file_pattern']).render(bkg_start_time=bkg_start_time, bkg_time=bkg_time))[0]
+					for file_process in common_config['background']['file_processes']:
+						run(Template(file_process).render(bkg_file=bkg_file, bkg_file_basename=os.path.basename(bkg_file), bkg_start_time=bkg_start_time, bkg_time=bkg_time))
+				except:
+					continue
+			os.chdir(wps_work_dir)
+			run(f'{wps_root}/link_grib.csh {wps_work_dir}/background/*')
 		else:
-			run('./ungrib.exe &> ungrib.out')
+			run(f'{wps_root}/link_grib.csh {bkg_dir}/*')
+		if args.verbose:
+			run(f'{wps_root}/ungrib/src/ungrib.exe')
+		else:
+			run(f'{wps_root}/ungrib/src/ungrib.exe &> ungrib.out')
 		if not check_files(expected_files):
 			if args.verbose:
 				cli.error('Failed!')
@@ -73,9 +109,11 @@ def run_wps(wps_root, gfs_root, config, args):
 	else:
 		cli.notice('File FILE:* already exist.')
 	run('ls -l FILE:*')
-	
+
+	# ------------------------------------------------------------------------------------------------
 	cli.notice('Run metgrid.exe ...')
-	expected_files = ['met_em.d01.{}.nc'.format(date.format('YYYY-MM-DD_HH:mm:ss')) for date in gfs_dates]
+	copy(f'{wps_root}/metgrid/src/metgrid.exe', 'metgrid.exe')
+	expected_files = ['met_em.d01.{}.nc'.format(time.format('YYYY-MM-DD_HH:mm:ss')) for time in bkg_times]
 	if not check_files(expected_files) or args.force:
 		# Remove possible existing met_em files.
 		run('rm -f met_em.*')
@@ -96,13 +134,23 @@ def run_wps(wps_root, gfs_root, config, args):
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description="Run WRF model by hiding operation details.\n\nLongrun Weather Inc., NWP operation software.\nCopyright (C) 2018 - All Rights Reserved.", formatter_class=argparse.RawTextHelpFormatter)
 	parser.add_argument('-c', '--codes', help='Root directory of all codes (e.g. WRF, WPS)')
+	parser.add_argument('-w', '--work-root',  dest='work_root', help='Work root directory')
 	parser.add_argument('-p', '--wps-root', dest='wps_root', help='WPS root directory (e.g. WPS)')
-	parser.add_argument('-b', '--gfs-root', dest='gfs_root', help='GFS root directory (e.g. gfs)')
+	parser.add_argument('-b', '--bkg-root', dest='bkg_root', help='Background root directory')
 	parser.add_argument('-j', '--config-json', dest='config_json', help='Configuration JSON file.')
 	parser.add_argument('-f', '--force', help='Force to run', action='store_true')
 	parser.add_argument('-v', '--verbose', help='Print out build log', action='store_true')
 	args = parser.parse_args()
-	
+
+	if not args.work_root:
+		if os.getenv('WORK_ROOT'):
+			args.work_root = os.getenv('WORK_ROOT')
+		else:
+			cli.error('Option --work-root or environment variable WORK_ROOT need to be set!')
+	args.work_root = os.path.abspath(args.work_root)
+	if not os.path.isdir(args.work_root):
+		cli.error(f'Directory {args.work_root} does not exist!')
+
 	if not args.wps_root:
 		if os.getenv('WPS_ROOT'):
 			args.wps_root = os.getenv('WPS_ROOT')
@@ -113,14 +161,14 @@ if __name__ == '__main__':
 	
 	args.wps_root = os.path.abspath(args.wps_root)
 	
-	if not args.gfs_root:
-		if os.getenv('GFS_ROOT'):
-			args.gfs_root = os.getenv('GFS_ROOT')
+	if not args.bkg_root:
+		if os.getenv('BKG_ROOT'):
+			args.bkg_root = os.getenv('BKG_ROOT')
 		else:
-			cli.error('Option --gfs-root or environment variable GFS_ROOT need to be set!')
+			cli.error('Option --bkg-root or environment variable BKG_ROOT need to be set!')
 	
-	args.gfs_root = os.path.abspath(args.gfs_root)
+	args.bkg_root = os.path.abspath(args.bkg_root)
 	
 	config = parse_config(args.config_json)
 
-	run_wps(args.wps_root, args.gfs_root, config, args)
+	run_wps(args.work_root, args.wps_root, args.bkg_root, config, args)
