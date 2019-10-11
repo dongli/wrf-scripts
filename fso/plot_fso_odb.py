43f927a0d6a890795bfe104acac4c7b5bf7ab890
Li Dong <dongli@longrunweather.com>
2019-10-11 14:05:10 +0800

    Fix plot_fso_odb.py
    QC 2 should be OK.

--- fso/plot_fso_odb.py
+++ fso/plot_fso_odb.py
@@ -11,7 +11,7 @@ parser = argparse.ArgumentParser(description="Plot FSO result from ODB files.", 
 parser.add_argument('-i', '--input-prefix', dest='input_prefix', help='Input ODB file prefix')
 args = parser.parse_args()
 
-file_paths = sorted(glob(args.input_prefix + '*'))
+file_paths = [file_path for file_path in sorted(glob(args.input_prefix + '*')) if not file_path.endswith('.pdf')]
 
 cmd = f'odb sql "select *" -T -i {file_paths[1]}'
 res = run(cmd, shell=True, stdout=PIPE, stderr=PIPE)
@@ -27,7 +27,7 @@ for line in res.stdout.decode('utf-8').strip().split('\n'):
 cmd = f'odb sql "select *" -T -i {file_paths[2]}'
 res = run(cmd, shell=True, stdout=PIPE, stderr=PIPE)
 if res.returncode != 0:
-	print('[Error]: Failed to run {cmd}!')
+	print(f'[Error]: Failed to run {cmd}!')
 	exit(1)
 
 var_impact = {}
@@ -36,13 +36,16 @@ for line in res.stdout.decode('utf-8').strip().split('\n'):
 	var_impact[var_type.replace("'", '')] = float(impact)
 
 p = (1000,950,900,850,800,750,700,650,600,550,500,400,300,200,100)
-lev_impact = { 'u': {}, 'v': {}, 't': {}, 'q': {} }
+sound_lev_impact = { 'u': {}, 'v': {}, 't': {}, 'q': {} }
 for k in range(len(p)):
 	for var in ('u', 'v', 't', 'q'):
-		lev_impact[var][p[k]] = []
-	p1 = p[k+1] if k != len(p) - 1 else p[-1] - 50
-	p2 = p[k-1] if k != 0 else p[0] + 50
-	cmd = f'odb sql \'select * where obs_type="sound" and p>={p1*100} and p<={p2*100}\' -T -i {file_paths[0]}'
+		sound_lev_impact[var][p[k]] = []
+	p1 = p[k] - 0.5 * (p[k] - (p[k+1] if k != len(p) - 1 else p[-1] - 50))
+	p2 = p[k] + 0.5 * ((p[k-1] if k != 0 else p[0] + 50) - p[k])
+	cmd = f'''
+odb sql \'select u_impact, u_qc, v_impact, v_qc, t_impact, t_qc, q_impact, q_qc
+where obs_type="sound" and p>={p1*100} and p<={p2*100}\' -T -i {file_paths[0]}
+'''
 	res = run(cmd, shell=True, stdout=PIPE, stderr=PIPE)
 	if res.returncode != 0:
 		print(f'[Error]: Failed to run {cmd}!')
@@ -51,16 +54,42 @@ for k in range(len(p)):
 		lev = {}
 		columns = line.split()
 		if len(columns) == 0:
-			print(f'[Error]: Empty record at {p[k]} pressure level!')
-			exit(1)
-		u_impact = columns[4]; u_qc = columns[5]
-		if u_impact != 'NULL' and u_qc == '0': lev_impact['u'][p[k]].append(float(u_impact))
-		v_impact = columns[9]; v_qc = columns[10]
-		if v_impact != 'NULL' and v_qc == '0': lev_impact['v'][p[k]].append(float(v_impact))
-		t_impact = columns[14]; t_qc = columns[15]
-		if t_impact != 'NULL' and t_qc == '0': lev_impact['t'][p[k]].append(float(t_impact))
-		q_impact = columns[24]; q_qc = columns[25]
-		if q_impact != 'NULL' and q_qc == '0': lev_impact['q'][p[k]].append(float(q_impact))
+			print(f'[Warning]: Empty record at {p[k]} pressure level!')
+			continue
+		u_impact = columns[0]; u_qc = columns[1]
+		if u_impact != 'NULL' and u_qc in ('0', '2'): sound_lev_impact['u'][p[k]].append(float(u_impact))
+		v_impact = columns[2]; v_qc = columns[3]
+		if v_impact != 'NULL' and v_qc in ('0', '2'): sound_lev_impact['v'][p[k]].append(float(v_impact))
+		t_impact = columns[4]; t_qc = columns[5]
+		if t_impact != 'NULL' and t_qc in ('0', '2'): sound_lev_impact['t'][p[k]].append(float(t_impact))
+		q_impact = columns[6]; q_qc = columns[7]
+		if q_impact != 'NULL' and q_qc in ('0', '2'): sound_lev_impact['q'][p[k]].append(float(q_impact))
+
+# p = (1000,950,900,850,800,750,700,650,600,550,500,400,300,200,100)
+# profiler_lev_impact = { 'u': {}, 'v': {} }
+# for k in range(len(p)):
+# 	for var in ('u', 'v'):
+# 		profiler_lev_impact[var][p[k]] = []
+# 	p1 = p[k] - 0.5 * (p[k] - (p[k+1] if k != len(p) - 1 else p[-1] - 50))
+# 	p2 = p[k] + 0.5 * ((p[k-1] if k != 0 else p[0] + 50) - p[k])
+# 	cmd = f'''
+# odb sql \'select u_impact, u_qc, v_impact, v_qc
+# where obs_type="profiler" and p>={p1*100} and p<={p2*100}\' -T -i {file_paths[0]}
+# '''
+# 	res = run(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+# 	if res.returncode != 0:
+# 		print(f'[Error]: Failed to run {cmd}!')
+# 		exit(1)
+# 	for line in res.stdout.decode('utf-8').strip().split('\n'):
+# 		lev = {}
+# 		columns = line.split()
+# 		if len(columns) == 0:
+# 			print(f'[Warning]: Empty record at {p[k]} pressure level!')
+# 			continue
+# 		u_impact = columns[0]; u_qc = columns[1]
+# 		if u_impact != 'NULL' and u_qc in ('0', '2'): profiler_lev_impact['u'][p[k]].append(float(u_impact))
+# 		v_impact = columns[2]; v_qc = columns[3]
+# 		if v_impact != 'NULL' and v_qc in ('0', '2'): profiler_lev_impact['v'][p[k]].append(float(v_impact))
 
 pdf = PdfPages(f'{args.input_prefix}.pdf')
 
@@ -80,17 +109,30 @@ plt.gca().yaxis.tick_right()
 plt.grid(True, color='gray', zorder=1)
 pdf.savefig()
 
-for var_type, levels in lev_impact.items():
+for var_type, levels in sound_lev_impact.items():
 	impact = []
 	for _, var_impact in levels.items():
 		impact.append(sum(var_impact))
 	fig = plt.figure(figsize=(6, 4))
 	plt.barh(list(levels.keys()), impact, color='blue', height=20, zorder=2)
-	plt.gca().set_xlabel(f'Impact of {var_type}')
+	plt.gca().set_xlabel(f'Impact of {var_type}@sound')
 	plt.gca().set_ylabel('Pressure Levels')
 	plt.gca().yaxis.tick_right()
 	plt.gca().invert_yaxis()
 	plt.grid(True, color='gray', zorder=1)
 	pdf.savefig()
+
+# for var_type, levels in profiler_lev_impact.items():
+# 	impact = []
+# 	for _, var_impact in levels.items():
+# 		impact.append(sum(var_impact))
+# 	fig = plt.figure(figsize=(6, 4))
+# 	plt.barh(list(levels.keys()), impact, color='blue', height=20, zorder=2)
+# 	plt.gca().set_xlabel(f'Impact of {var_type}@profiler')
+# 	plt.gca().set_ylabel('Pressure Levels')
+# 	plt.gca().yaxis.tick_right()
+# 	plt.gca().invert_yaxis()
+# 	plt.grid(True, color='gray', zorder=1)
+# 	pdf.savefig()
 
 pdf.close()
