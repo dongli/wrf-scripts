edb92b3c5f41e6c18e6cbf8414b80d38d01c423d
Li Dong <dongli@longrunweather.com>
2020-05-29 20:21:12 +0800

    Fix upp_version.py

--- utils/upp_version.py
+++ utils/upp_version.py
@@ -6,6 +6,8 @@ from packaging.version import Version, parse
 def upp_version(upp_root):
 	if os.path.isfile(f'{upp_root}/arch/version'):
 		line = open(f'{upp_root}/arch/version', 'r').readline()
+	elif os.path.isfile(f'{upp_root}/version'):
+		line = open(f'{upp_root}/version', 'r').readline()
 	else:
 		cli.error('Could not find UPP version!')
 	match = re.search('UPPV(\d+\.\d+(\.\d+)?)', line)[1]
